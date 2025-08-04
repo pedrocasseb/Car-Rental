761bb2c7e67fd9a6d0aabdf0c2649beb6d8509c7
pedrocasseb <casseb.phcc@gmail.com>
2025-08-04 12:44:28 -0300

    feat: connect backend and frontend, login and change role to owner

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useAppContext } from '../context/AppContext';
 
-const Login = ({ setShowLogin }) => {
+const Login = () => {
+
+    const {setShowLogin, axios, setToken, navigate, toast} = useAppContext()
 
     const [state, setState] = React.useState("login");
     const [name, setName] = React.useState("");
@@ -8,7 +11,21 @@ const Login = ({ setShowLogin }) => {
     const [password, setPassword] = React.useState("");
 
     const onSubmitHandler = async (event)=>{
-        event.preventDefault();
+        try {
+            event.preventDefault();
+            const {data} = await axios.post(`/api/user/${state}`, {name, email, password})
+            if(data.success){
+                navigate('/')
+                setToken(data.token)
+                localStorage.setItem('token', data.token)
+                setShowLogin(false)
+            } else {
+                toast.error(data.message)
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+        
     }
 
   return (
