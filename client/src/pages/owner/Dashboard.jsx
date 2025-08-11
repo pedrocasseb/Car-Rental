2af10b5ecd2534cbda1b4bf422a0e7ff0e42481f
pedrocasseb <casseb.phcc@gmail.com>
2025-08-11 15:00:53 -0300

    feat(client): connect my bookings to backend

--- client/src/pages/owner/Dashboard.jsx
+++ client/src/pages/owner/Dashboard.jsx
@@ -81,7 +81,7 @@ const Dashboard = () => {
           ))}
         </div>
         <div className='p-4 md:p-6 mb-6 border border-borderColor rounded-md w-full md:max-w-xs'>
-          <h1 className='text-lg font-medium'>Montly Revenue</h1>
+          <h1 className='text-lg font-medium'>Monthly Revenue</h1>
           <p className='text-gray-500'>Revenue for current month</p>
           <p className='text-3xl mt-6 font-semibold text-primary'>{currency} {data.monthlyRevenue}</p>
         </div>
